d0ef04cd3e789ab1ceb57a32c3ce5ce2285e0e52
Rufus Pollock <rufus.pollock@okfn.org>
2012-04-30 04:15:48 +0100

    [process,mapping][s]: add a mapping for this dataset so faceting works correctly.

--- process.py
+++ process.py
@@ -1,10 +1,14 @@
 import csv
 import logging
+import json
 import datastore.client
 
 url = 'http://datahub.io/dataset/land-matrix/resource/f46a9192-cf2b-410e-b4f7-dc80538e5541'
 client = datastore.client.DataStoreClient(url)
 fp = 'data/database.csv'
+mappingfp = 'data/database.csv.mapping.json'
+mapping = json.load(open(mappingfp))
+print mapping
 
 def upload():
     # use Deal Number as unique id 
@@ -13,6 +17,7 @@ def upload():
         return dict_
     # in general should not need to delete since have unique id
     client.delete()
+    client.mapping_update(mapping)
     client.upsert(map(add_id, csv.DictReader(open(fp), delimiter=';')))
 
 if __name__ == '__main__':
